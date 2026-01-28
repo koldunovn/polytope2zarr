5d5b013b7d27c235f86e99c05d704a14838d2926
koldunovn <koldunovn@gmail.com>
2026-01-28 13:51:09 +0100

    add some examples

---
+++ job_climsight_hist.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name=CS_hist
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --exclusive
+#SBATCH --time=08:00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --account=ab0995
+#SBATCH --output=timmean_%j.out
+#SBATCH -e timmean_%j.err
+
+# limit stacksize and core file size
+ulimit -s 204800
+ulimit -c 0
+
+echo "Processing years ${START_YEAR} to ${END_YEAR}"
+echo "Job started at $(date)"
+
+# Run the python script
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235165 --levtype sfc --model ifs-fesom
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235165 --levtype sfc --model icon
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235165 --levtype sfc --model ifs-nemo
+
+
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235166 --levtype sfc --model ifs-fesom
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235166 --levtype sfc --model icon
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235166 --levtype sfc --model ifs-nemo
+
+
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 228004 --levtype sfc --model ifs-fesom
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 228004 --levtype sfc --model icon
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 228004 --levtype sfc --model ifs-nemo
+
+
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235055 --levtype sfc --model ifs-fesom
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235055 --levtype sfc --model icon
+python download_to_zarr.py --stream clmn --year-range 1990 2014 --param 235055 --levtype sfc --model ifs-nemo
+ 
+echo "Job finished at $(date)"
---
+++ job_nemo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=nemo
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --exclusive
+#SBATCH --time=08:00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --account=ab0995
+#SBATCH --output=timmean_%j.out
+#SBATCH -e timmean_%j.err
+
+# limit stacksize and core file size
+ulimit -s 204800
+ulimit -c 0
+
+echo "Processing years ${START_YEAR} to ${END_YEAR}"
+echo "Job started at $(date)"
+
+# Run the python script
+python download_to_zarr.py --stream clte --year-range 1990 2014 --param 263124 --model ifs-fesom
+
+echo "Job finished at $(date)"
